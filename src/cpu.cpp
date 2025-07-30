4fdf206ed2666de03b0cb4621c3f7b25a459d318
Leandro Barcelos Ferreira <leandrobarcelos.f@hotmail.com>
2025-07-30 18:45:17 -0300

    cpu: implement LUI instruction

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,11 +1,24 @@
 #include "cpu.h"
 #include <format>
+#include <iostream>
+#include <gsl/gsl>
 
 void cpu::CPU::cycle() {
   const uint32_t instruction = load(program_counter_);
   program_counter_ += cpu::INSTRUCTION_LENGTH;
 
-  switch (instruction) {
+  switch (instruction >> 26U & 0x3FU) {
+    case LUI_OPCODE: {
+      uint32_t register_index = instruction >> 16U & 0x1FU;
+      uint16_t immediate = instruction & 0xFFFFU;
+
+      std::cout << std::format(
+          "lui {:02X}, {:04X}",
+          instruction, register_index, immediate) << '\n';
+
+      gsl::at(registers_, register_index) = immediate << 16U;
+      break;
+    }
     default:
       throw std::runtime_error(
           std::format("unhandled instruction {:08X}", instruction));
--- src/cpu.h
+++ src/cpu.h
@@ -1,11 +1,13 @@
 #ifndef POLYSTATION_CPU_H_
 #define POLYSTATION_CPU_H_
+#include <array>
 #include "bios.h"
 #include "bus.h"
 
 namespace cpu {
 constexpr uint32_t NUMBER_OF_REGISTERS = 32;
 constexpr uint32_t INSTRUCTION_LENGTH = 4;
+constexpr uint32_t LUI_OPCODE = 0x0F;
 
 class CPU {
 public:
@@ -16,6 +18,7 @@ public:
 
 private:
   uint32_t program_counter_ = bios::BIOS_BASE;
+  std::array<uint32_t, NUMBER_OF_REGISTERS> registers_{};
   bus::Bus bus_;
 
   [[nodiscard]] uint32_t load(uint32_t address) const;
