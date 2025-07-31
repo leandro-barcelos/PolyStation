8529ec4baaf142b8a7e70c8dcb51421734758960
Leandro Barcelos Ferreira <leandrobarcelos.f@hotmail.com>
2025-07-31 10:18:21 -0300

    cpu: refactor opcode to functions

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -9,44 +9,15 @@ void cpu::CPU::Cycle() {
   program_counter_ += cpu::kInstructionLength;
 
   switch (instruction.GetOpcode()) {
-    case Instruction::Opcode::kORI: {
-      uint32_t register_s = instruction.GetRegisterS();
-      uint32_t register_t = instruction.GetRegisterT();
-      uint16_t immediate = instruction.GetImmediate16();
-
-      std::cout << std::format("ori {:02X}, {:02X}, {:04X}", register_t,
-                               register_s, immediate)
-                << '\n';
-
-      gsl::at(registers_, register_t) =
-          gsl::at(registers_, register_s) | immediate;
-
+    case Instruction::Opcode::kORI:
+      OpORI(instruction);
       break;
-    }
-    case Instruction::Opcode::kLUI: {
-      uint32_t register_t = instruction.GetRegisterT();
-      uint16_t immediate = instruction.GetImmediate16();
-
-      std::cout << std::format("lui {:02X}, {:04X}", register_t, immediate)
-                << '\n';
-
-      gsl::at(registers_, register_t) = immediate << 16U;
+    case Instruction::Opcode::kLUI:
+      OpLUI(instruction);
       break;
-    }
-    case Instruction::Opcode::kSW: {
-      uint32_t register_s = instruction.GetRegisterS();
-      uint32_t register_t = instruction.GetRegisterT();
-      uint16_t immediate = instruction.GetImmediate16();
-
-      const uint32_t address = gsl::at(registers_, register_s) + immediate;
-      Store(address, gsl::at(registers_, register_t));
-
-      std::cout << std::format("sw {:02X}, {:04X}({:02X})", register_t,
-                               immediate, register_s)
-                << '\n';
-
+    case Instruction::Opcode::kSW:
+      OpSW(instruction);
       break;
-    }
     default:
       throw std::runtime_error(
           std::format("unhandled instruction {:03X}",
@@ -57,8 +28,43 @@ void cpu::CPU::Cycle() {
 uint32_t cpu::CPU::Load(const uint32_t address) const {
   return bus_.Load(address);
 }
+
 void cpu::CPU::Store(const uint32_t address, const uint32_t value) {
   bus::Bus::Store(address, value);
+}
+
+void cpu::CPU::OpORI(const Instruction& instruction) {
+  uint32_t register_s = instruction.GetRegisterS();
+  uint32_t register_t = instruction.GetRegisterT();
+  uint16_t immediate = instruction.GetImmediate16();
+
+  gsl::at(registers_, register_t) = gsl::at(registers_, register_s) | immediate;
+
+  std::cout << std::format("ori {:02X}, {:02X}, {:04X}", register_t, register_s,
+                           immediate)
+            << '\n';
+}
+
+void cpu::CPU::OpLUI(const Instruction& instruction) {
+  uint32_t register_t = instruction.GetRegisterT();
+  uint16_t immediate = instruction.GetImmediate16();
+
+  gsl::at(registers_, register_t) = immediate << 16U;
+
+  std::cout << std::format("lui {:02X}, {:04X}", register_t, immediate) << '\n';
+}
+
+void cpu::CPU::OpSW(const Instruction& instruction) {
+  uint32_t register_s = instruction.GetRegisterS();
+  uint32_t register_t = instruction.GetRegisterT();
+  uint16_t immediate = instruction.GetImmediate16();
+
+  const uint32_t address = gsl::at(registers_, register_s) + immediate;
+  Store(address, gsl::at(registers_, register_t));
+
+  std::cout << std::format("sw {:02X}, {:04X}({:02X})", register_t, immediate,
+                           register_s)
+            << '\n';
 }
 
 cpu::Instruction::Opcode cpu::Instruction::GetOpcode() const {
--- src/cpu.h
+++ src/cpu.h
@@ -9,21 +9,6 @@ namespace cpu {
 constexpr uint32_t kNumberOfRegisters = 32;
 constexpr uint32_t kInstructionLength = 4;
 
-class CPU {
- public:
-  explicit CPU(const std::string& path) : bus_(path) {}
-
-  void Cycle();
-
- private:
-  uint32_t program_counter_ = bus::kBiosBase;
-  std::array<uint32_t, kNumberOfRegisters> registers_{};
-  bus::Bus bus_;
-
-  [[nodiscard]] uint32_t Load(uint32_t address) const;
-  static void Store(uint32_t address, uint32_t value);
-};
-
 class Instruction {
  public:
   explicit Instruction(const uint32_t instruction) : data_(instruction) {}
@@ -37,6 +22,25 @@ class Instruction {
 
  private:
   uint32_t data_;
+};
+
+class CPU {
+ public:
+  explicit CPU(const std::string& path) : bus_(path) {}
+
+  void Cycle();
+
+ private:
+  uint32_t program_counter_ = bus::kBiosBase;
+  std::array<uint32_t, kNumberOfRegisters> registers_{};
+  bus::Bus bus_;
+
+  [[nodiscard]] uint32_t Load(uint32_t address) const;
+  static void Store(uint32_t address, uint32_t value);
+
+  void OpORI(const Instruction& instruction);
+  void OpLUI(const Instruction& instruction);
+  void OpSW(const Instruction& instruction);
 };
 }  // namespace cpu
 
